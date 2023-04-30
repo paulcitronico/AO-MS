761ad1316289f53d769e02379f3865b8e2875a65
Javier garnica <jgarnica2019@alu.uct.cl>
2023-04-30 18:09:54 -0400

    simplex 2 fases - reconoce columna pivote
    se implementó método que reconoce la columna pivote, hace la fase 1 completa , tambien se implementa funcion que corrige los ceros en notacion cientifica a ceros naturales, falta: ampliar Z, detalles menores

--- simplex_m2fases.py
+++ simplex_m2fases.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
 import numpy as np
+from decimal import Decimal, getcontext
+
 
 #restricciones = 60x1 + 60x2 >= 300, 12x1 + 6x2 >= 36,10x1 + 30x2 >= 90
 
@@ -12,6 +14,7 @@ def ampliar_restricciones():
 
     #introducir las restricciones separadas por coma
     restricciones = input("Introducir las restricciones separadas por coma: ")
+    #fz = input("Introducir la funcion Z: ")
     restricciones = [r.strip() for r in restricciones.split(",")]
     
     for i, restriccion in enumerate(restricciones):
@@ -82,22 +85,22 @@ def obtener_coeficientes(restricciones, variables):
         dicc_variables[variable] = i
 
     for restriccion in restricciones:
-        lista_coeficientes = [0] * len(variables)           # lista para guardar los coeficientes
-        tuplas_coeficientes = re.findall(patron, restriccion)
+        lista_coeficientes = [0] * len(variables)               # lista para guardar los coeficientes
+        tuplas_coeficientes = re.findall(patron, restriccion)   # busca con re.findall y crea las tuplas con los valores numericos(coeficientes)
 
         for tupla in tuplas_coeficientes:   
-            if tupla[0] == '':                  # En caso de que no exista un coeficiente numerico se asume que es 1, 
+            if tupla[0] == '':                             # En caso de que no exista un coeficiente numerico se asume que es 1, 
                 coeficiente = 1
-            elif tupla[0] == '-':               # si el coeficiente es un guión, se asume que es -1, de lo contrario,
+            elif tupla[0] == '-':                          # si el coeficiente es un guión, se asume que es -1, de lo contrario,
                 coeficiente = -1
             else:
-                coeficiente = float(tupla[0])   # se asigna el valor numérico que tenga asociado
+                coeficiente = float(tupla[0])              # se asigna el valor numérico que tenga asociado
 
             # se ordenan los valores segun el orden de las variables previamente dadas
             nombre_variable = tupla[1]
-            if nombre_variable in dicc_variables:                       # si existe el nombre de la variable dentro del diccionario previamente creado
+            if nombre_variable in dicc_variables:                           # si existe el nombre de la variable dentro del diccionario previamente creado
                 posicion_variable = dicc_variables[nombre_variable]     
-                lista_coeficientes[posicion_variable] = coeficiente     # se agrega a la lista de coeficientes el del coeficiente actual
+                lista_coeficientes[posicion_variable] = coeficiente         # se agrega a la lista de coeficientes el del coeficiente actual
 
         # se agrega el valor del lado derecho a la lista de coeficientes
         constante = float(restriccion.split('=')[1].strip())                #se separa la constante por el simbolo =
@@ -121,7 +124,7 @@ def seleccionar_variables_artificiales(variables,nombre_variable):
 
 # funcion que obtiene las variables segun su tipo (letra con que comienza a,e,h,x) ( recibe la matriz )
 def seleccionar_variables_por_nombre(matriz,nombre_variable):
-    variables = matriz.columns                              #obtiene el nombre de las columnas de la matriz
+    variables = matriz.columns                                                              # obtiene el nombre de las columnas de la matriz
     variables_artificiales = [var for var in variables if var.startswith(nombre_variable)]  # busca dentro de la lista de variables la que comienze con la letra asignada
     return variables_artificiales
 
@@ -135,7 +138,7 @@ def seleccionar_columna(matriz,nombre_columna):
 # funcion que obtiene una fila en base al numero de esta
 def seleccionar_fila(matriz,pos_fila):
     nombre_fila = matriz.index[pos_fila]    #busca el nombre asignado en base a su posicion
-    fila = matriz.loc[nombre_fila]     # selecciona la fila con el nombre asignado( a1,a2,x1,x2,etc )
+    fila = matriz.loc[nombre_fila]          # selecciona la fila con el nombre asignado( a1,a2,x1,x2,etc )
     return fila
 #===========================================================================================================================
 
@@ -148,15 +151,18 @@ def crear_matriz():
    
     # se crea la funcion Z con las variables artificiales
     cadena_valores = ' + '.join(var_artificiales)                           # Crear la cadena de caracteres usando la función join()
-    z = f'-Z + {cadena_valores} = 0'                                         # Crear la cadena final usando un f-string
+    z = f'Z + {cadena_valores} = 0'                                         # Crear la cadena final usando un f-string
     restricciones.insert(0,z)                                               #se agrega la funcion z a la lista de restricciones
 
     coeficientes = obtener_coeficientes(restricciones,variables)            # se obtienen la lista de valores numericos
 
     # formar los nombres para las filas      
     holguras = seleccionar_variables_artificiales(variables,'h')            # se obtiene una lista de las variables de holgura
-    filas = var_artificiales + holguras                                         # se juntan las listas 
+    filas = var_artificiales + holguras                                     # se juntan las listas 
     filas.insert(0,'Z')                                                     # se agrega la variable Z al inicio de la lista
+
+    print(restricciones)
+    print(variables)
 
     # crear dataframe con los datos
     matriz = pd.DataFrame(coeficientes, columns=variables, index=filas)
@@ -168,7 +174,7 @@ def crear_matriz():
 
 # restricciones = xeA + xoA >= 40, xeB + xoB >= 70, 2xeA + 2xoA <= xeB + xoB, xeA + xeB <= 180, xoA + xoB <= 45, xij >= 0
 # restricciones = xeA + xoA >= 40, xeB + xoB >= 70, xeA + xeB <= 180, xoA + xoB <= 45, xij >= 0
-# restricciones = x1 + x2 >= 40, x3 + x4 >= 70, x1 + x3 <= 180, x2 + x4 <= 45, x5 >= 0
+# restricciones = x1 + x2 >= 40, x3 + x4 >= 70,2x1 + 2x2 - x3 - x4 <= 0, x1 + x3 <= 180, x2 + x4 <= 45, x5 >= 0
 #variables = ['Z', 'e1', 'a1', 'x1', 'e2', 'a2', 'x2', 'e3', 'a3', 'LD']
 
 #===========================================================================================================================
@@ -179,7 +185,7 @@ def obtener_fila_pivote(matriz,columna):
     col = seleccionar_columna(matriz,columna)       # obtiene una columna entera del dataframe(matriz)
     # ciclo para dividir el lado derecho por la columna pivote 
     indice_minimo = float(0)
-    division_previa = 1000000000000000000
+    division_previa = 100000000000000000000000
     for i in range(len(LD)):
         if(col[i]>float(0) and LD[i]>float(0)):    # evitar divisones por 0 y negativos
             division = float(LD[i] / col[i])
@@ -195,6 +201,8 @@ def reduccion_gauss(matriz,columna):
     # guardar datos de la matriz actual para luego crear la nueva
     variables = matriz.columns
     index = matriz.index
+
+    getcontext().prec = 4           # se limitan los decimales
 
     columna_pivote = seleccionar_columna(matriz, columna)       # selecciona la columna pivote
     num_fila_pivote = obtener_fila_pivote(matriz, columna)      # obtiene la posicion de la fila pivote
@@ -213,10 +221,12 @@ def reduccion_gauss(matriz,columna):
         if i!=num_fila_pivote:                  # solo si NO es la fila pivote
             fila_aux = seleccionar_fila(matriz,i)
 
+            # ciclo que hace la operacion
             for j in range(len(fila_aux)):
-                mult = col_piv[i]*fila_pivote[j]
-                fila_aux[j] = fila_aux[j] - mult
-            matriz_aux.insert(i,fila_aux)
+                mult = Decimal(col_piv[i])*Decimal(fila_pivote[j])          # se utiliza Decimal para limitar la cantidad de decimales a operar
+                fila_aux[j] = Decimal(fila_aux[j])-Decimal(mult)
+
+            matriz_aux.insert(i,fila_aux)       # se inserta el valor obtenido en la posicion indicada
 
             fila_aux = []     # vaciar la fila auxiliar.
         # en caso de ser la fila pivote, guardar la fila directamente en la matriz auxiliar
@@ -227,20 +237,43 @@ def reduccion_gauss(matriz,columna):
     
     # crear nueva matriz con los nuevos datos
     nueva_matriz = pd.DataFrame(matriz_aux, columns=variables, index=index)
-    print(nueva_matriz)
 
+    nueva_matriz = nueva_matriz.applymap(convertir_cero)
+    print(nueva_matriz)
     return nueva_matriz
 #===========================================================================================================================
+
+# funcion especial que se utiliza para convertir los ceros en notacion cientifica de la matriz en ceros, ( se aplica a la matriz mediante applymap tal que : matriz.applymap(convertir_cero) )
+def convertir_cero(x):
+    if isinstance(x, Decimal) and "E+" in str(x):       #verifica si existe notacion cientifica en el valor
+        if x.is_zero():                                 # si el valor es cero
+            return Decimal(0)                           # retornar el numero convertido a 0 decimal
+    return x                                            # en caso contrario retornar el mismo valor previo
+
 # funcion que busca el valor minimo de una fila ( recibe una matriz y la posicion de la fila)
 def buscar_columna_pivote(matriz, pos_fila):
     fila_z = seleccionar_fila(matriz, pos_fila)     # seleccionar la fila Z
     fila_z.pop('LD')                                # eliminar la columna LD
-    minimo = min(fila_z)                            # buscar el valor minimo
-    indice_minimo = fila_z.idxmin()                 # buscar el indice del valor minimo
+    fila_z.pop('Z')                                 # eliminar columna Z, para evitar que se detecte un negativo en caso de minimizacion
+    minimo = min(fila_z)                            # buscar el valor minimo dentro de la fila
 
-    return indice_minimo                            # retornar el indice (nombre de la columna)
+    indice_minimo = fila_z.astype(float).idxmin()   # buscar el indice del valor minimo ( se convierte la fila en float para que el metodo idxmin() reconozca los datos)
+
+    if minimo < 0 :                                 # si el valor minimo es negativo
+        return [indice_minimo,True]                 # retornar el indice (nombre de la columna)  y una flag True         
+    else:                                           
+        return [indice_minimo,False]                # si es positivo retornar flag = False
                       
 #===========================================================================================================================
+def ampliar_funcion_z(fz):
+    # entrada :  z = '-Z + 0.12x1 + 0.15x2 = 0'
+    # salida  :  z = [-1,0.12,0.0,0.15,0.0,0.0,0]
+
+    
+    funcion_z = 0
+    return funcion_z
+
+
 # funcion que crea la matriz de la fase 2, ( recibe la matriz y la funcion Z ampliada en forma de lista ordenada segun las variables)
 def crear_2da_matriz(matriz, z_ampliado):
     var_artificiales = seleccionar_variables_por_nombre(matriz,'a')
@@ -262,21 +295,14 @@ for i in range(len(columnas_artificiales)):
 
 #restricciones = 60x1 + 60x2 >= 300, 12x1 + 6x2 >= 36,10x1 + 30x2 >= 90
 
-#while any(i < 0 for i in fila_z):
-#negativo = True
-#for i in range(10):
- #   columna_pivote = buscar_columna_pivote(matriz,0)        # buscar columna pivote en la fila Z
-  #  matriz = reduccion_gauss(matriz,columna_pivote[0])         # reduccion gaussiana
-   # negativo = columna_pivote[1]
+negativo = True
+while negativo == True:
+    columna_pivote = buscar_columna_pivote(matriz,0)            # buscar columna pivote en la fila Z
+    negativo = columna_pivote[1]                                # verfica si la columna pivote tiene un valor negativo
+    if negativo == True:                                        # si es negativo se hace la reduccion
+        matriz = reduccion_gauss(matriz,columna_pivote[0])      # reduccion gaussiana
 
-variables = [ 'x2','x1','e2', 'x1', 'x2']
-
-matriz = reduccion_gauss(matriz,variables[0])
-matriz = reduccion_gauss(matriz,variables[1])
-matriz = reduccion_gauss(matriz,variables[2])
-matriz = reduccion_gauss(matriz,variables[3])
-matriz = reduccion_gauss(matriz,variables[4])
 
 #z = '-Z + 0.12x1 + 0.15x2 = 0'
-z = [-1,0.12,0.0,0.15,0.0,0.0,0]
-crear_2da_matriz(matriz, z)
+#z = [-1,0.12,0,0.15,0,0,0]
+#crear_2da_matriz(matriz, z)
